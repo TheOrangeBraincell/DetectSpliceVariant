8274b540d9c84b50f5b93244cbb0090749a0b2d1
Mirjam <mirjam.karlssonmueller@gmail.com>
2023-07-05 11:12:00 +0200

    initialized new PSI score script

---
+++ PsiScores.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Date: Wed Jul  5 11:06:17 2023
+File Name:
+Author: Mirjam Karlsson-Müller
+
+Description:
+    
+    
+List of Functions:
+    
+Procedure: 
+    1.
+    2.
+    3.
+    
+Useage:
+    
+    
+Possible Bugs:
+"""
+
