f2d8bda053865bb26d1d2ae3935c52701059d633
Mirjam <mirjam.karlssonmueller@gmail.com>
2023-07-19 13:03:52 +0200

    CE implementation in new script complete, AD in progress

--- New_PSI_test.py
+++ New_PSI_test.py
@@ -110,16 +110,120 @@ with open(args.input, "r") as asfile:
             gene=[e.strip(" ") for e in line.strip("\n").split(",")]
             gene[3]=int(gene[3])
             gene[4]=int(gene[4])
+            print(gene)
             continue
         
         #All thats left now is AS entries.
         Location, AS_Type, Gene_name, Database, Gencode_ID, Refseq_ID= line.strip().split("\t")
-        #Initialize dictionary
-        AS_events[AS_Type+"_"+Location]=dict()
+        #Initialize dictionary, different for AA/AD then for IR/CE
+        if AS_Type.startswith("A"):
+            if AS_Type+"_"+Location.split("_")[0] not in AS_events:
+                AS_events[AS_Type+"_"+Location.split("_")[0]]={Location.split("_")[1:]: dict()}
+            else:
+                AS_events[AS_Type+"_"+Location.split("_")[0]][Location.split("_")[1:]]=dict()
+        else:
+            AS_events[AS_Type+"_"+Location]=dict()
 
 #%% 2. Read in list of bam files. 
 
 print("Reading in BAM files...", end="\r")
+
+def CE(sample, event, read):
+    CE_start=int(event.split("_")[3])
+    CE_stop=int(event.split("_")[4])
+    
+    #read information
+    read_start=int(read.reference_start)
+    read_range=sum([int(i) for i in re.findall(r'\d+', read.cigarstring)])
+    read_stop=read_start+read_range
+    
+    #If both start and stop of read are before or after CE, then we can go on.
+    if read_start < CE_start and read_stop < CE_start:
+        return False
+    if read_start > CE_stop and read_stop > CE_stop:
+        return False
+    
+    #We are in the right range, lets process the read.
+    current_cigar = read.cigarstring
+    read_name=read.query_name
+    #Spliced or not?
+    if re.search(r'\d+M(?:\d+[I,D,S,H])?\d+N(?:\d+[I,D,S,H])?\d+M',read.cigarstring):
+        #This is spliced, but its could have insertions and/or deletions around the intron... 
+        if not re.search(r'\d+M\d+N\d+M',read.cigarstring):
+            #we save excluded weird reads like this one for the logfile.
+            excluded_reads.add(read_name)
+            #Then this read doesnt need to be processed at all.
+            return True
+        
+        current_start = read_start
+        #Otherwise we process it as spliced
+        while re.search(r'(\d+)M(\d+)N(\d+)M', current_cigar):
+            #assign splice junction variables
+            junction = re.search(r'(\d+)M(\d+)N(\d+)M', current_cigar)
+            
+            #assign variables to groups.
+            exon1 = int(junction.group(1))
+            intron=int(junction.group(2))
+            exon2=int(junction.group(3))
+            exon1_start = current_start
+            exon1_end = exon1_start+exon1+1  #exclusive
+            exon2_start = exon1_end+intron -1 #inclusive
+            exon2_end=exon2_start+exon2+1
+                
+            #skip alignments with less than 3 matching bases in an exon.
+            if exon1<3 or exon2<3:
+                # update cigar string
+                current_cigar = re.sub(r'^.*?N', 'N', current_cigar).lstrip("N")
+                current_start= exon2_start
+                return False
+            
+            #Number of spliced reads from previous to CE.
+            if exon1_end< CE_start and CE_stop >= exon2_end>=CE_start:
+                #count!
+                if read_name not in AS_events[event]["IR"]["reads"]["spliced"]:
+                    AS_events[event]["IR"]["count"]+=1
+                    AS_events[event]["IR"]["reads"]["spliced"].append(read_name)
+                    if read_name in AS_events[event]["IR"]["reads"]["not"]:
+                        #That means we have counted this reads pair as exon read. 
+                        #But we prioriize spliced reads in the counts.
+                        AS_events[event]["IR"]["count_n"]-=1
+                        AS_events[event]["IR"]["reads"]["not"].remove(read_name)
+                
+            #Number of spliced reads from CE to next.   
+            elif CE_start <= exon1_start<=CE_stop and exon2_start >CE_stop:
+                #count!
+                if read_name not in AS_events[event]["IR"]["reads"]["spliced"]:
+                    AS_events[event]["IR"]["count"]+=1
+                    AS_events[event]["IR"]["reads"]["spliced"].append(read_name)
+                    if read_name in AS_events[event]["IR"]["reads"]["not"]:
+                        #That means we have counted this reads pair as exon read. 
+                        #But we prioriize spliced reads in the counts.
+                        AS_events[event]["IR"]["count_n"]-=1
+                        AS_events[event]["IR"]["reads"]["not"].remove(read_name)
+                    
+            #Number of spliced reads accross CE.
+            elif exon1_end<CE_start and exon2_start>CE_stop:
+                #count!
+                if read_name not in AS_events[event]["ER"]["reads"]:
+                    AS_events[event]["ER"]["count"]+=1
+                    AS_events[event]["ER"]["reads"].append(read_name)
+                    
+            # update cigar string
+            current_cigar = re.sub(r'^.*?N', 'N', current_cigar).lstrip("N")
+            current_start= exon2_start
+    
+    #Otherwise the read is not spliced and we need to count it if its within the CE.
+    else:
+        #read information
+        read_start=int(read.reference_start)
+        read_range=sum([int(i) for i in re.findall(r'\d+', read.cigarstring)])
+        read_stop=read_start+read_range
+        read_name=read.query_name
+        
+        #Since we already excluded all reads in the wrong range and spliced are counted, we can just count these.
+        if read_name not in AS_events[event]["IR"]["reads"]["spliced"] and read_name not in AS_events[event]["IR"]["reads"]["not"]:
+            AS_events[event]["IR"]["count_n"]+=1
+            AS_events[event]["IR"]["reads"]["not"].append(read_name)
 
 def PSI_for_Sample(sample):
     #Gene information to open bam file.
@@ -132,9 +236,29 @@ def PSI_for_Sample(sample):
     for event in AS_events:
         if event.startswith("CE"):
             AS_events[event]={"ER":{"count":0, "reads":[]}, 
-                                         "IR":{"count":0, "reads":[], "count_n":0, 
+                                         "IR":{"count":0, "reads":{"spliced":[], "not":[]}, "count_n":0, 
                                                "normalize":int(event.split("_")[4])-int(event.split("_")[3])}}
-    
+        elif event.startswith("AD"):
+            #dict: {AD_#: {start1: dict, start2: dict}}
+            starts=sorted(list(AS_events[event].keys()))
+            for start in AS_events[event]:
+                #Calculate normalize as difference between previous start and current.
+                current_index=starts.index(start)
+                #This next part is only true for plus strand. figure out minus strand
+                
+                "YOU ARE HERE!"
+                
+                if current_index==0:
+                    #Its the earliest start, it wont have any difference reads. hence normalization=1
+                    normalize=1
+                else:
+                    previous_index=current_index-1
+                    current_start=int(AS_events[event][start].split("_")[3])
+                    previous_start=int(AS_events[event][starts[previous_index]].split("_")[3])
+                    normalize=current_start-previous_start
+                AS_events[event][start]={"ER":{"count":0, "reads":{"spliced":[], 
+                                                                   "not":[]},
+                                               "count_n":0, "normalize":normalize}}
     #open the bam file, and go through the reads.
     samfile=pysam.AlignmentFile(files[sample], 'rb', index_filename=files[sample][0:-1]+"i")
     reads=samfile.fetch(chrom, gene_start, gene_stop)
@@ -144,103 +268,24 @@ def PSI_for_Sample(sample):
         #Filter
         if Filter_Reads(read, gene_strand)==True:
             continue
-    
-        #read information
-        read_start=int(read.reference_start)
-        read_range=sum([int(i) for i in re.findall(r'\d+', read.cigarstring)])
-        read_stop=read_start+read_range
         
         #Now we go through all the events and try to figure out what to count them for.
         for event in AS_events:
             #event=event id later used in first column of output.
             #Extract CE info from event key.
             if event.startswith("CE"):
-                CE_start=int(event.split("_")[3])
-                CE_stop=int(event.split("_")[4])
-                
-                #If both start and stop of read are before or after CE, then we can go on.
-                if read_start < CE_start and read_stop < CE_start:
-                    continue
-                if read_start > CE_stop and read_stop > CE_stop:
-                    continue
-                
-                #We are in the right range, lets process the read.
-                current_cigar = read.cigarstring
-                read_name=read.query_name
-                #Spliced or not?
-                if re.search(r'\d+M(?:\d+[I,D,S,H])?\d+N(?:\d+[I,D,S,H])?\d+M',read.cigarstring):
-                    #This is spliced, but its could have insertions and/or deletions around the intron... 
-                    if not re.search(r'\d+M\d+N\d+M',read.cigarstring):
-                        #we save excluded weird reads like this one for the logfile.
-                        excluded_reads.add(read_name)
-                        continue
-                    
-                    current_start = int(read_start)
-                    #Otherwise we process it as spliced
-                    while re.search(r'(\d+)M(\d+)N(\d+)M', current_cigar):
-                        #assign splice junction variables
-                        junction = re.search(r'(\d+)M(\d+)N(\d+)M', current_cigar)
-                        
-                        #assign variables to groups.
-                        exon1 = int(junction.group(1))
-                        intron=int(junction.group(2))
-                        exon2=int(junction.group(3))
-                        exon1_start = current_start
-                        exon1_end = exon1_start+exon1+1  #exclusive
-                        exon2_start = exon1_end+intron -1 #inclusive
-                        exon2_end=exon2_start+exon2+1
-                            
-                        #skip alignments with less than 3 matching bases in an exon.
-                        if exon1<3 or exon2<3:
-                            # update cigar string
-                            current_cigar = re.sub(r'^.*?N', 'N', current_cigar).lstrip("N")
-                            current_start= exon2_start
-                            continue
-                        
-                        #Number of spliced reads from previous to CE.
-                        if exon1_end< CE_start and CE_stop >= exon2_end>=CE_start:
-                            #count!
-                            if read_name not in AS_events[event]["IR"]["reads"]:
-                                AS_events[event]["IR"]["count"]+=1
-                                AS_events[event]["IR"]["reads"].append(read_name)
-                            
-                        #Number of spliced reads from CE to next.   
-                        elif CE_start <= exon1_start<=CE_stop and exon2_start >CE_stop:
-                            #count!
-                            if read_name not in AS_events[event]["IR"]["reads"]:
-                                AS_events[event]["IR"]["count"]+=1
-                                AS_events[event]["IR"]["reads"].append(read_name)
-                                
-                        #Number of spliced reads accross CE.
-                        elif exon1_end<CE_start and exon2_start>CE_stop:
-                            #count!
-                            if read_name not in AS_events[event]["ER"]["reads"]:
-                                AS_events[event]["ER"]["count"]+=1
-                                AS_events[event]["ER"]["reads"].append(read_name)
-                                
-                        # update cigar string
-                        current_cigar = re.sub(r'^.*?N', 'N', current_cigar).lstrip("N")
-                        current_start= exon2_start
-                
-                #Otherwise the read is not spliced and we need to count it if its within the CE.
-                else:
-                    #read information
-                    read_start=int(read.reference_start)
-                    read_range=sum([int(i) for i in re.findall(r'\d+', read.cigarstring)])
-                    read_stop=read_start+read_range
-                    read_name=read.query_name
-                    
-                    #Since we already excluded all reads in the wrong range and spliced are counted, we can just count these.
-                    if read_name not in AS_events[event]["IR"]["reads"]:
-                        AS_events[event]["IR"]["count_n"]+=1
-                        AS_events[event]["IR"]["reads"].append(read_name)
-                        
+                skip=CE(sample, event, read)
+                if skip==True:
+                    #The read is invalid all together, so we want to break the inner loop.
+                    break
+                #Else the read just doesnt match the current event and we let the program continue.
             elif event.startswith("AA"):
                 
-                
-                
                 continue
+            
             elif event.startswith("AD"):
+                
+                
                 continue
             elif event.startswith("IR"):
                 continue
@@ -249,8 +294,7 @@ def PSI_for_Sample(sample):
     sample_PSI={"sample":sample}
     for event in AS_events:
         if event.startswith("CE"):
-            print(sample, event, AS_events[event]["IR"]["count"], AS_events[event]["IR"]["count_n"], AS_events[event]["ER"]["count"])
-            #PSI is NAN if there is 10 or less reads.
+            #PSI is NAN if there are 10 or less reads.
             if AS_events[event]["IR"]["count"]+AS_events[event]["IR"]["count_n"]+AS_events[event]["ER"]["count"]<11:
                 PSI="NAN"
             else:
@@ -288,7 +332,7 @@ for sample in sample_names:
 
 print("Reading in BAM files: Done! \n", end="\r")
 
-#%% 3. Read counts, per sample, allow parallelization
+#%% 3. Read counts, per sample, allow parallelization, write output.
 
 excluded_reads=set()
 
@@ -309,8 +353,8 @@ with open(args.out, "w") as outfile:
         outfile.write(new_line+"\n")
     
 
-#%% 4. Write output
+#%% End time
 
-
+print("Run time: {:.2f} seconds.".format(time.time()-start_time))  
 
 
